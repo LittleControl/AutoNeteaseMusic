d076c294bf2cc937ab56f2c3dc8206a1203bd5d7
littlecontrol <i@littlecontrol.me>
2021-10-26 17:34:09 +0800

    feat: 多国手机号登陆

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const CONFIG_DIR = path.join(__dirname, "config")
  * @description: 获取api
  */
 const getLocalApi = async () => {
-  let api
-  try {
-    api = await readFile(`${CONFIG_DIR}/api`, "utf-8")
-  } catch (error) {
-    api = "https://api.littlecontrol.me"
-  }
-  return api
+  // let api
+  // try {
+  //   api = await readFile(`${CONFIG_DIR}/api`, "utf-8")
+  // } catch (error) {
+  //   api = "https://api.littlecontrol.me"
+  // }
+  // return api
+  return "https://api.littlecontrol.me"
 }
 
 /**
@@ -53,19 +54,15 @@ axios.interceptors.request.use(
  * @description: 从文件读取账户信息
  */
 const getAccountInfo = async () => {
-  try {
-    const res = await readFile(`${CONFIG_DIR}/account`, "utf-8")
-    const resArr = res.split("\r\n")
-    const parsedNumber = parsePhoneNumber(resArr[0], "CN")
-    const phone = parsedNumber?.formatNational().replace(/[() -]/g, "")
-    const countrycode = parsedNumber?.countryCallingCode
-    return {
-      phone,
-      countrycode,
-      password: resArr[1],
-    }
-  } catch (error) {
-    return error
+  const res = await readFile(`${CONFIG_DIR}/account`, "utf-8")
+  const resArr = res.split("\r\n")
+  const parsedNumber = parsePhoneNumber(resArr[0], "CN")
+  const phone = parsedNumber?.formatNational().replace(/[()\s-]/g, "")
+  const countrycode = parsedNumber?.countryCallingCode
+  return {
+    phone,
+    countrycode,
+    password: resArr[1],
   }
 }
 
@@ -74,19 +71,15 @@ const getAccountInfo = async () => {
  */
 const loginByPhone = async () => {
   let res
-  try {
-    const api = await getLocalApi()
-    const url = `${api}/login/cellphone`
-    const accountInfo = await getAccountInfo()
-    const { data } = await axios({
-      method: "POST",
-      url,
-      data: accountInfo,
-    })
-    res = data?.cookie ?? ""
-  } catch (error) {
-    res = ""
-  }
+  const api = await getLocalApi()
+  const url = `${api}/login/cellphone`
+  const accountInfo = await getAccountInfo()
+  const { data } = await axios({
+    method: "POST",
+    url,
+    data: accountInfo,
+  })
+  res = data?.cookie ?? ""
   return res
 }
 
@@ -95,18 +88,14 @@ const loginByPhone = async () => {
  */
 
 const getCookie = async () => {
-  try {
-    const localCookie = await readFile(`${CONFIG_DIR}/cookie`, "utf8")
-    const { data } = await getLoginStatus(localCookie)
-    if (data.account && data.profile) {
-      return localCookie
-    } else {
-      const COOKIE = await loginByPhone()
-      await writeFile(`${CONFIG_DIR}/cookie`, COOKIE)
-      return COOKIE
-    }
-  } catch (error) {
-    return error
+  const localCookie = await readFile(`${CONFIG_DIR}/cookie`, "utf8")
+  const { data } = await getLoginStatus(localCookie)
+  if (data.account && data.profile) {
+    return localCookie
+  } else {
+    const COOKIE = await loginByPhone()
+    await writeFile(`${CONFIG_DIR}/cookie`, COOKIE)
+    return COOKIE
   }
 }
 
@@ -142,23 +131,22 @@ const getUserLevelInfo = async (api) => {
  * @description: 打卡签到
  */
 const checkIn = async (api) => {
+  console.log("checkin in")
   const url = `${api}/daily_signin`
   let res
-  try {
-    res = await axios({
-      method: "POST",
-      url,
-    })
-    res = await axios({
-      method: "POST",
-      url,
-      params: {
-        type: 1,
-      },
-    })
-  } catch (error) {
-    res = error
-  }
+  res = await axios({
+    method: "POST",
+    url,
+  })
+  console.log("web checkin fin")
+  res = await axios({
+    method: "POST",
+    url,
+    params: {
+      type: 1,
+    },
+  })
+  console.log("android check fin")
   return res.data ?? res
 }
 
@@ -196,24 +184,20 @@ const playDailyLists = async (api) => {
   const url = `${api}/scrobble`
   const { recommend } = await getDailyPlaylist(api)
   let res
-  try {
-    recommend.forEach(async (item) => {
-      const { privileges } = await getPlaylistContent(item.id, api)
-      privileges.forEach(async (song) => {
-        res = await axios({
-          method: "POST",
-          url,
-          params: {
-            id: song.id,
-            sourceid: item.id,
-            time: 500,
-          },
-        })
+  recommend.forEach(async (item) => {
+    const { privileges } = await getPlaylistContent(item.id, api)
+    privileges.forEach(async (song) => {
+      res = await axios({
+        method: "POST",
+        url,
+        params: {
+          id: song.id,
+          sourceid: item.id,
+          time: 500,
+        },
       })
     })
-  } catch (error) {
-    res = error
-  }
+  })
   return res
 }
 
@@ -236,21 +220,17 @@ const playDailySongs = async (api) => {
   const url = `${api}/scrobble`
   const { dailySongs } = await getDailySongs(api)
   let res
-  try {
-    dailySongs.forEach(async (song) => {
-      res = await axios({
-        method: "POST",
-        url,
-        params: {
-          id: song.id,
-          sourceid: song.al.id,
-          time: 500,
-        },
-      })
+  dailySongs.forEach(async (song) => {
+    res = await axios({
+      method: "POST",
+      url,
+      params: {
+        id: song.id,
+        sourceid: song.al.id,
+        time: 500,
+      },
     })
-  } catch (error) {
-    res = error
-  }
+  })
   return res
 }
 
@@ -258,29 +238,32 @@ const playDailySongs = async (api) => {
  * @description: 云贝签到
  */
 const checkInYunbei = async (api) => {
+  console.log("yunbei in")
   const url = `${api}/yunbei/sign`
   let res
-  try {
-    res = await axios({
-      method: "POST",
-      url,
-    })
-  } catch (error) {
-    res = error
-  }
+  res = await axios({
+    method: "POST",
+    url,
+  })
+  console.log("yunbei out")
   return res
 }
+
+// checkInYunbei("https://api.littlecontrol.me").then(
+//   (res) => console.log(res),
+//   (error) => console.log(error)
+// )
 
 export const main = async () => {
   let res
   try {
     const api = await getLocalApi()
-    res = await checkIn(api)
-    res = await checkInYunbei(api)
     res = await playDailySongs(api)
     res = await playDailyLists(api)
+    res = await checkIn(api)
+    res = await checkInYunbei(api)
   } catch (error) {
-    res = error
+    res = error?.response?.data
   }
   return res
 }
